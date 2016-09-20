d62c43dbcc2b31a8c287e2ceb80b510bf705da7f
Shirlette Chambers <shirlette.chambers@gmail.com>
2016-09-19 20:30:08 -0400

    Animations added to classes using plugins

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@
 // You can use Isotope as a jQuery plugin: $('selector').isotope().
 $( document ).ready(function() {
 
-$('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
-});
+// $('.grid').isotope({
+//   // options
+//   itemSelector: '.grid-item',
+//   layoutMode: 'fitRows'
+// });
 
 $('body').scrollspy({ target: '#bs-example-navbar-collapse-1' })
 
